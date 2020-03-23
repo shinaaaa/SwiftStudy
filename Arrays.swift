3de900984b9689f45f9e81d2715647215f280d44
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-03-24 00:12:26 +0900

    배열

--- Arrays.swift
+++ Arrays.swift
@@ -9,7 +9,7 @@ print(len)
 //3. 배열 순회
 for row in arrays {
     var index = arrays.index(of: row)
-    print("\(index) 번째 \(row)")
+    print("\(index!) 번째 \(row)")
 }
 
 //4. 배열 동적 선언
@@ -28,3 +28,14 @@ var arrays4 = [String]()
 var arrays5 : [String]
 arrays5 = [String]()
 // arrays5 = ()
+
+//5. 배열 동적 추가
+// append(_:) 배열 맨뒤에 추가
+// insert(_:at:) 원하는 위치에 추가
+// append(contentsOf:) 맨뒤에 여러개 추가
+var arrays6 = [String]()
+arrays6.append("A")
+arrays6.append("C")
+arrays6.insert("B", at: 1)
+arrays6.append(contentsOf: ["D","E"])
+print(arrays6)
