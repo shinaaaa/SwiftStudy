3c8d7e0084a095f88c215a5395fe89aef343a69b
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-03-30 21:01:53 +0900

    While

---
+++ While/While.swift
@@ -0,0 +1,21 @@
+//1. While 구문
+var n = 2
+
+while n < 10 {
+    n = n * 2
+    print(n)
+}
+
+//2. 무한 루프
+while true {
+    
+}
+
+//3. repeat while 구문
+var n = 2
+
+repeat {
+    n = n * 2
+    print(n)
+}
+while n < 10
