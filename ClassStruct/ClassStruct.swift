0d2cb789c0d575e193920b78c24f92345431bd61
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-04-02 22:24:39 +0900

    클래스 구조체

---
+++ ClassStruct/ClassStruct.swift
@@ -0,0 +1,15 @@
+// 클래스/구조체 선언
+class ClTest1 {
+    var a = 10
+    var b = 20
+}
+struct StTest1 {
+    var a = 11
+    var b = 22
+}
+
+// 인스턴스 생성
+let InClTest1 = ClTest1()
+print(InClTest1.a)
+let InStTest1 = StTest1()
+print(InStTest1.a)
