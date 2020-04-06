740b86e07cabe7ebedf64c2c7629a70b4f502b2f
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-04-06 23:32:38 +0900

    프로퍼티 초기화 구문

--- ClassStruct/ClassStruct.swift
+++ ClassStruct/ClassStruct.swift
@@ -25,3 +25,23 @@ print(InStTest1.a)
 // - 특정 연산을 통해 값을 만들어 제공
 // - 변수만 사용해서 정의 기능
 // - 클래스, 구조체, 열거형 사용 가능
+
+// 1. 초기화 구문
+class Car {
+    var color: String
+    
+    init() {
+        self.color = ""
+    }
+}
+
+// 2. 옵셔널이 타입 설정
+class Car2 {
+//    var color: String?
+    var color: String!
+}
+
+// 3. 프로퍼티에 초기값 설정
+class Car3 {
+    var color: String = ""
+}
