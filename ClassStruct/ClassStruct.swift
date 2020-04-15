d42ec419f4ccc8e150b844e62f06f655d7c6c282
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-04-15 22:56:30 +0900

    메소드

--- ClassStruct/ClassStruct.swift
+++ ClassStruct/ClassStruct.swift
@@ -133,3 +133,36 @@ class let bye: String {
         
     }
 }
+
+// 메소드
+// 특정 타입의 객체 내부에서 사용하는 함수
+
+// 인스턴스 메소드
+// - 주어진 객체의 인스턴스와 함께 수행
+// - 객체 타입으로 호출 불가능
+// - 인스턴스 생성후 호출 가능
+
+struct reSolution {
+    var w = 0;
+    var h = 0;
+    func desc() -> String {
+        let desc = "가로는 \(self.w) 세로는 \(self.h)"
+        return desc
+    }
+}
+// 호출
+var res = reSolution()
+res.w = 10
+res.h = 20
+print(res.desc())
+
+// 타입 메소드
+// - 객체 타입 자체에 관련된 메소드
+
+class Foo {
+    class func fooType() {
+        
+    }
+}
+// 호출
+Foo.fooType()
