3c8f1afae1e3d4529ff58636c1cdc58bb6874899
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-04-22 23:28:32 +0900

    열거형

--- Enumeration/Enumeration.swift
+++ Enumeration/Enumeration.swift
@@ -1,3 +1,27 @@
 // 열거형
 // 하나의 주제로 연관된 데이터들이 멤버로 구성되어 있는 자료형 객체
-// 
+// enum 열거형 이름 {
+// case 멤버값
+// }
+enum Dic {
+    case north
+    case south
+    case east, west
+}
+
+let N = Dic.north
+let S = Dic.south
+
+var dicW: Dic = Dic.west
+var dicE: Dic = .east
+
+switch dicW {
+    case Dic.west :
+        print("서쪽")
+    case Dic.east :
+        print("동쪽")
+    case Dic.north :
+        print("북쪽")
+    case Dic.south :
+        print("남쪽")
+}
