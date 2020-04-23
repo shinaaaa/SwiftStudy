98b923d6d4e5aabbd23a6f12a367209fe20e2a5c
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-04-23 22:54:40 +0900

    멤버와 값 불리

--- Enumeration/Enumeration.swift
+++ Enumeration/Enumeration.swift
@@ -25,3 +25,27 @@ switch dicW {
     case Dic.south :
         print("남쪽")
 }
+
+// 값 분리
+enum HTTPCode: Int {
+    case OK = 200
+    case NOT_MODIFY = 304
+    case INCORRECT_PAGE = 404
+    case SERVER_ERROR = 500
+}
+print(HTTPCode.OK.rawValue)
+
+// 자동 할당
+enum Rank: Int {
+    case one = 1
+    case two, three
+}
+print(Rank.one.rawValue)
+print(Rank.three.rawValue)
+
+// 연관 값
+enum ImageFormat {
+    case jpg
+    case png(Bool)
+    case gif(Int, Bool)
+}
