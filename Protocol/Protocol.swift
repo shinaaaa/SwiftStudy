98a50098f85367a6fb0c781e597c4e8bfe9f6f53
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-04-28 22:50:02 +0900

    프로토콜

--- Protocol/Protocol.swift
+++ Protocol/Protocol.swift
@@ -1,1 +1,20 @@
+// 프로토콜
+// 구현해야 할 메소드의 명세가 작성
+// 델리게이트 패턴에서 이벤트에 대한 위임 처리를 하기 위함
+// 구현할 메소드의 형식을 공통으로 정의
+// 구현한 객제의 메소드나 속성을 은닉하고 프로토콜에서 선언된 명세의 내용만 제공
+// protocol 프로토콜명 { 구현할 메소드 명세 }
+// struct/class/enum/extention 객체명: 구현할 프로토콜명 { }
+protocol SomePropertyProtocol {
+    var name: String { get set }
+    var description: String { get }
+}
 
+struct RubyMember: SomePropertyProtocol {
+    var name = "홍길동"
+    var description: String {
+        return "NAME : \(self.name)"
+    }
+}
+var ruby = RubyMember()
+print(ruby.description)
